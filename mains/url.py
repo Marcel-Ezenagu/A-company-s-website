737ffe41cd2676c8ca104fd707ab52e4f30db083
MARCEL <MARCELEZENAGU92@GMAIL.COM>
2020-08-15 04:59:52 +0100

    first commit

---
+++ mains/url.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from products import views
+
+urlpatterns = [
+    path('', views.product_index, name='product_index'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
+    
+]
